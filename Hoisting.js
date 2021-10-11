aac7a1dc27b418e6a7a646381acda286f861b0da
MD Pear E Habib <habib.austcse31@gmail.com>
2021-10-11 10:19:14 +0600

    variable hoisting complete

--- Hoisting.js
+++ Hoisting.js
@@ -1,2 +1,32 @@
+//scenario 1
+//cosole.log(a);   
 
-cosole.log(a);
+//output:  ReferencError
+
+//scenario 2
+//console.log(a);
+//var a="Bangladesh";
+
+
+//output:  undefined
+
+//scenario 3
+//console.log(a);
+//let a="Bangladesh";
+
+
+//output:  ReferencError
+
+
+//scenario 4
+//var a;
+//console.log(a);
+
+//output:  undefined
+
+
+//scenario 5
+let a;
+console.log(a);
+
+//output: undefined
