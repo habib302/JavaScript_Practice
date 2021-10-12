ef70d3a8c2ef41a9d308f6d52954305bd6ed344c
MD Pear E Habib <habib.austcse31@gmail.com>
2021-10-12 14:52:21 +0600

    promise new way

--- Asynchoronous.js
+++ Asynchoronous.js
@@ -66,7 +66,33 @@
 
 //scenario 4: Promises
 
-const hasMeeting=false;
+// const hasMeeting=false;
+
+// const meeting = new Promise((resolve,reject)=>{
+//     if(hasMeeting){
+//         const meetingDetails={
+//             name:'technical meeting',
+//             location:'google meet',
+//             time:'2.00pm'
+//         }
+//         resolve(meetingDetails);
+//     }
+//     else{
+//         reject(new Error('meeting already schedule'));
+//     }
+// })
+
+// meeting.then((res)=>{
+//             console.log(res);
+//         })
+//         .catch((err)=>{
+//             console.log(err);
+//         })
+
+
+//scenario 5: Promises another way to create
+
+const hasMeeting=true;
 
 const meeting = new Promise((resolve,reject)=>{
     if(hasMeeting){
@@ -82,9 +108,26 @@ const meeting = new Promise((resolve,reject)=>{
     }
 })
 
-meeting.then((res)=>{
-            console.log(res);
-        })
-        .catch((err)=>{
-            console.log(err);
-        })
+// const addToCalendar=(meetingDetails)=>{
+//     return new Promise((resolve,reject)=>{
+//         const calendar = `${meetingDetails.name} has been scheduled on ${meetingDetails.location} at ${meetingDetails.time}`;
+//         resolve(calendar);
+//     })
+// }
+
+//another way to create promise
+const addToCalendar=(meetingDetails)=>{ 
+    
+    const calendar = `${meetingDetails.name} has been scheduled on ${meetingDetails.location} at ${meetingDetails.time}`;
+    return Promise.resolve(calendar);
+    
+}
+
+meeting
+.then(addToCalendar)
+.then((res)=>{
+    console.log(res);
+})
+.catch((err)=>{
+    console.log(err);
+})
