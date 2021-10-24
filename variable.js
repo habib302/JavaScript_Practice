4ab52ca35bddcc8a9a71b9ff6587787860868b3d
Md Pear E Habib <habib.austcse31@gmail.com>
2021-10-24 08:33:23 +0600

    variable

---
+++ variable.js
@@ -0,0 +1,22 @@
+
+//var variable
+// var name="habib var 1"
+// console.log(name);
+
+// var name="habib2 var 2"
+// console.log(name);
+
+
+//let variable
+// let name1="habib let 1"
+// console.log(name);
+
+// let name1="habib2 let 2"
+// console.log(name);
+
+//const variable
+const name2="habib const 1"
+console.log(name);
+
+name2="habib2 const 2"
+console.log(name);
